9e864bbce259c8b334476cdaaa2e7a1fe216cb3a
Gelber <gelber.mahecha@skyline.be>
2023-03-15 08:31:40 -0500

    Add project files.

---
+++ GitHub-DeploymentPackage-Test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHub-DeploymentPackage-Test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("GitHub-DeploymentPackage-Test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("F153A961-C5F0-471C-8F53-61866827B144")]
+[assembly: AssemblyVersion("1.0.0.0")]
